3101f4e011627c86a0f42218739e873b1dd2c694
DiegoFelipe7 <90659322+DiegoFelipe7@users.noreply.github.com>
2021-12-09 16:18:15 -0500

    cambio

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
-import Login from './components/Login';
-import Home from './components/views/Home';
-import Usuarios from './components/views/Usuarios';
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import RegistroUsuarios from "./components/RegistroUsuarios"
+import Login from "./components/Login";
+import Home from "./components/views/Home";
+import Usuarios from "./components/views/Usuarios";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import RegistroUsuarios from "./components/RegistroUsuarios";
 function App() {
   return (
-   <>
+    <>
       <BrowserRouter>
-          <Routes>
-            <Route exact path="/" element={<Login/>}/>
-            <Route exact path="menu/home" element={<Home/>}/>
-            <Route exact path="menu/usuarios" element={<Usuarios/>}/>
-            <Route exact path="Registro" element={<RegistroUsuarios/>}/>
-          </Routes>
-          
-      </BrowserRouter>    
-   </>
+        <Routes>
+          <Route exact path="/" element={<Login />} />
+          <Route exact path="menu/home" element={<Home />} />
+          <Route exact path="menu/usuarios" element={<Usuarios />} />
+          <Route exact path="registro" element={<RegistroUsuarios />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
